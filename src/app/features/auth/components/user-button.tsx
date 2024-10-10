84f08793740f641e94159c89b727aec7c838a37a
smais007 <smaisshawon@gmail.com>
2024-10-11 03:26:36 +0600

    user button added 👻

--- src/app/features/auth/components/user-button.tsx
+++ src/app/features/auth/components/user-button.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Loader } from "lucide-react";
+import { Loader, LogOut } from "lucide-react";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -17,6 +17,7 @@ import { useLogout } from "../api/use-logout";
 
 export const UserButton = () => {
   const { data: user, isLoading } = useCurrent();
+  const { mutate: logout } = useLogout();
   if (isLoading) {
     return (
       <div className="size-10 rounded-full flex items-center justify-center  bg-neutral-200 border border-neutral-200 ">
@@ -43,6 +44,33 @@ export const UserButton = () => {
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-60"
+        align="end"
+        side="bottom"
+        sideOffset={10}
+      >
+        <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
+          <Avatar className="size-[52px]  transition border border-neutral-300">
+            <AvatarFallback className="bg-neutral-200 text-2xl font-medium text-neutral-500 flex items-center justify-center">
+              {avatarFallback}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm font-medium text-neutral-900">
+              {name || "User"}
+            </p>
+            <p className="text-xs text-neutral-500">{email}</p>
+          </div>
+        </div>
+        <DropdownMenuSeparator className="mb-1" />
+        <DropdownMenuItem
+          onClick={() => logout()}
+          className="h-10 flex items-center justify-center font-medium cursor-pointer "
+        >
+          <LogOut className="size-4 mr-2" /> Log Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,8 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useCurrent } from "./features/auth/api/use-current";
-import { useEffect } from "react";
-import { Button } from "@/components/ui/button";
-import { useLogout } from "./features/auth/api/use-logout";
 import { UserButton } from "./features/auth/components/user-button";
 
 export default function Home() {
-  const router = useRouter();
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
-
-  useEffect(() => {
-    if (!isLoading && !data) {
-      router.push("/sign-in");
-    }
-  }, [data, isLoading, router]);
-
   return (
     <div className="flex gap-4 p-20 ">
-      <h1>Only visiable for authenticated users</h1>
-      <Button onClick={() => mutate()}>Logout</Button>
       <UserButton />
     </div>
   );
